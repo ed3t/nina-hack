5476cf52d73820f4d1eba3df3d5063865a905412
ed3t <ever.asap@gmail.com>
2025-05-07 18:11:29 +0100

    Setup project with Vite

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    laravel({
+      input: [
+        'resources/js/app.js',
+        'resources/css/app.css',
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+  },
+});
