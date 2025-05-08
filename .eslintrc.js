6bee10a6591becfa1cfd20c1d7cc9e49fe77a946
ed3t <ever.asap@gmail.com>
2025-05-08 15:05:17 +0100

    chore: add linting

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:vue/vue3-recommended',
+      'airbnb-base',
+      'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: ['vue', 'prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+    },
+  };
